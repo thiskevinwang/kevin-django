f0d6c4564121924b795eca3f39a91df25c17452b
Kevin Wang <kwangsan@gmail.com>
2019-10-07 01:28:40 -0400

    add `board_topics` function based view (FBV)

--- kdjango/boards/views.py
+++ kdjango/boards/views.py
@@ -14,3 +14,9 @@ def home(request):
 	response_html = '<br>'.join(boards_names)
 
 	return HttpResponse(response_html)
+
+# PART 3
+# https://simpleisbetterthancomplex.com/series/2017/09/18/a-complete-beginners-guide-to-django-part-3.html#using-the-urls-api
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
