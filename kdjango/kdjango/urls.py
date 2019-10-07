21caff5a259a400d3e27d4f47edf0f7b080b99e9
Kevin Wang <kwangsan@gmail.com>
2019-10-07 01:26:59 -0400

    add topics.html

--- kdjango/kdjango/urls.py
+++ kdjango/kdjango/urls.py
@@ -27,4 +27,8 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     # or
     # url(r'^$', views.home, name='home'),
+
+    url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/', admin.site.urls),
 ]
