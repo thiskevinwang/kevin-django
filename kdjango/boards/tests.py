42cc726ae461344bb517e77cffa1dea4c6ad899b
Kevin Wang <kwangsan@gmail.com>
2019-10-07 01:41:15 -0400

    add 1 test

--- kdjango/boards/tests.py
+++ kdjango/boards/tests.py
@@ -1,3 +1,10 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+# add 1 test
+# https://simpleisbetterthancomplex.com/series/2017/09/11/a-complete-beginners-guide-to-django-part-2.html#testing-the-homepage
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
