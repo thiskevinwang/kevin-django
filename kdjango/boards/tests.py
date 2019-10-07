b2e85ec86fb3db80a5e9d265a116ec284d1e6d76
Kevin Wang <kwangsan@gmail.com>
2019-10-07 01:42:03 -0400

    test command comment

--- kdjango/boards/tests.py
+++ kdjango/boards/tests.py
@@ -3,6 +3,11 @@ from django.test import TestCase
 
 # add 1 test
 # https://simpleisbetterthancomplex.com/series/2017/09/11/a-complete-beginners-guide-to-django-part-2.html#testing-the-homepage
+# 
+'''
+cd kdjango
+python manage.py test
+'''
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
