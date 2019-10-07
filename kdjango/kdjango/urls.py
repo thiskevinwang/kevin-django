68d911bfabe7208c2c43bdbd17fd552b5d9ff2ed
Kevin Wang <kwangsan@gmail.com>
2019-10-07 01:31:46 -0400

    comment-out non-unique URLs

--- kdjango/kdjango/urls.py
+++ kdjango/kdjango/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     # or
     # url(r'^$', views.home, name='home'),
 
-    url(r'^$', views.home, name='home'),
+    # url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
 ]
