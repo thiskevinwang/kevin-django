57471a1ac208c01a46b8fce8ed13c56c02d41f4d
Kevin Wang <kwangsan@gmail.com>
2019-10-08 19:17:41 -0400

    enable signup & redirect

--- kdjango/accounts/views.py
+++ kdjango/accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        # If the form is valid...
+        if form.is_valid():
+            # a *User* instance is created 
+            user = form.save()
+            # The created user is passed as an argument to the
+            # auth `login` function, manually authenticating the user.
+            login(request, user)
+            # After that, the view redirects the user to the homepage,
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
