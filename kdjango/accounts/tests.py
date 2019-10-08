c0dafa13979616e9cfe2d0cab578373e4e872f4d
Kevin Wang <kwangsan@gmail.com>
2019-10-07 23:35:36 -0400

    add signup tests

--- kdjango/accounts/tests.py
+++ kdjango/accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from .views import signup
 
-# Create your tests here.
+class SignUpTests(TestCase):
+    def test_signup_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
