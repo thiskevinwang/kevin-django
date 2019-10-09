314c8ff06163c6036d852252fb1053e0a6ad0641
Kevin Wang <kwangsan@gmail.com>
2019-10-08 22:15:03 -0400

    add template tag tests

--- kdjango/boards/tests/test_views.py
+++ kdjango/boards/tests/test_views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import User
 from django.urls import resolve, reverse
 from django.test import TestCase
 
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 
 class HomeTests(TestCase):
